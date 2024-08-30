ab62a35ee79c76d884a170363e5721c853ebaba3
dineshmeel <dineshmeel123@gmail.com>
2024-08-30 23:08:43 +0530

    apis to create update and delete invoice and authentication

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,11 @@
-export default function Page() {
-    return <p>Customers Page</p>;
-  }
+import { fetchFilteredCustomers } from "@/app/lib/data";
+import CustomersTable from "@/app/ui/customers/table";
+
+export default async function Page() {
+  const customers = await fetchFilteredCustomers("")
+  return (
+    <main>
+      <CustomersTable customers={customers}></CustomersTable>
+    </main>
+  );
+}
